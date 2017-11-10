44048382554a4c1f1dfdfe287f70bb1c8955b011
marcoss90 <marcoscabral90@gmail.com>
2017-11-10 19:50:25 -0300

    Este examen es coherente.

--- tests.py
+++ tests.py
@@ -8,19 +8,19 @@ class BasicTestsCase(unittest.TestCase):
     def test_1(self):
 
         self.assertTrue(1.5 > 1)
-        self.assertTrue(False)
+        self.assertTrue(True)
         self.assertTrue(11 > 1.5)
         self.assertFalse(11 < 1)
-        self.assertNotEqual(11, 10 + 1)
-        self.assertNotEqual(12, 10 + 2)
+        self.assertNotEqual(11, 10 + 4)
+        self.assertNotEqual(12, 10 + 3)
 
     def test_2(self):
-        self.assertNotIn(1, [1, 2, 3])
-        self.assertNotIn(1, [1, 1, 1])
+        self.assertNotIn(1, [3, 4, 5])
+        self.assertNotIn(1, [6, 7, 9])
         self.assertNotIn(1, [2, 2, 2])
         self.assertIn('a', 'abcd')
-        self.assertIn('z', 'abcd')
-        self.assertNotIn('a', 'abcd')
+        self.assertIn('z', 'abcz')
+        self.assertNotIn('a', 'bcdd')
 
 
 class LinuxCommandsTestsCase(unittest.TestCase):
@@ -28,9 +28,9 @@ class LinuxCommandsTestsCase(unittest.TestCase):
     def test_1(self):
         parametros_de_ls = ['-a', '-l', '-x', '-la']
 
-        self.assertIn('l', parametros_de_ls)
-        self.assertIn('a', parametros_de_ls)
-        self.assertNotIn('r', parametros_de_ls)
+        self.assertIn('-a', parametros_de_ls)
+        self.assertIn('-l', parametros_de_ls)
+        self.assertNotIn('x', parametros_de_ls)
 
     def _num_to_perm(self, num):
         # Don't touch this method
@@ -39,18 +39,18 @@ class LinuxCommandsTestsCase(unittest.TestCase):
 
     def test_2(self):
         c = self._num_to_perm('777')
-        self.assertEqual(c, '')
+        self.assertEqual(c, 'rwxrwxrwx')
         c = self._num_to_perm('775')
-        self.assertEqual(c, '-x')
+        self.assertEqual(c, 'rwxrwxr-x')
         c = self._num_to_perm('644')
-        self.assertEqual(c, '')
+        self.assertEqual(c, 'rw-r--r--')
 
     def test_3(self):
-        c = self._num_to_perm('')
+        c = self._num_to_perm('400')
         self.assertEqual(c, 'r--------')
-        c = self._num_to_perm('')
+        c = self._num_to_perm('555')
         self.assertEqual(c, 'r-xr-xr-x')
-        c = self._num_to_perm('')
+        c = self._num_to_perm('744')
         self.assertEqual(c, 'rwxr--r--')
 
 
@@ -60,8 +60,8 @@ class GitCommandsTestsCase(unittest.TestCase):
         valid_commands = ['add', 'rm', 'commit', 'pull', 'pull']
 
         self.assertEqual('git ' + valid_commands[0], 'git add')
-        self.assertEqual('git ' + valid_commands[1], '')
-        self.assertEqual('git ' + valid_commands[2], '')
-        self.assertEqual('git ' + valid_commands[3], 'git add')
+        self.assertEqual('git ' + valid_commands[1], 'git rm')
+        self.assertEqual('git ' + valid_commands[2], 'git commit')
+        self.assertEqual('git ' + valid_commands[3], 'git pull')
 
 unittest.main()
